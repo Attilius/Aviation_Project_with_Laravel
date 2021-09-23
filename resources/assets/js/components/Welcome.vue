e172607d0ee51555ff1ae4aee9939bb685a7707b
Attilius <leo20@freemail.hu>
2021-09-23 21:33:35 +0200

    I added scripts section and a form

--- resources/assets/js/components/Welcome.vue
+++ resources/assets/js/components/Welcome.vue
@@ -1,17 +1,53 @@
 <template>
     <div class="wrapper">
-        <div class="content container-sm">
+        <div class="content">
             <div class="under-left">
                 <div class="left">
-                <img :src="'images/A320.png'" />
+                    <b-img v-bind="mainProps" :src="'images/A320.png'"></b-img>>
                 </div>
             </div>
             <div class="right">
+                <h1>FLY WITH US</h1>
+                <b-form inline>
+                    <label class="sr-only" for="inline-form-input-name"
+                        >Name</label
+                    >
+                    <b-form-input
+                        id="inline-form-input-name"
+                        class="mb-2 mr-sm-2 mb-sm-0"
+                        placeholder="Jane Doe"
+                    ></b-form-input>
 
+                    <label class="sr-only" for="inline-form-input-username"
+                        >Username</label
+                    >
+                    <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+                        <b-form-input
+                            id="inline-form-input-username"
+                            placeholder="Username"
+                        ></b-form-input>
+                    </b-input-group>
+
+                    <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0"
+                        >Remember me</b-form-checkbox
+                    >
+
+                    <b-button variant="primary">Save</b-button>
+                </b-form>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mainProps: { class: 'm1 m2 m3 m4 m5'}
+      }
+    }
+  }
+</script>
 
 <style>
 .wrapper {
@@ -24,11 +60,12 @@
     margin-top: 80px;
     display: flex;
     justify-content: space-between;
-    border: 2px solid rgb(155, 187, 236);
+    background: whitesmoke;
 }
 .under-left {
     height: 80vh;
     width: 40%;
+    background: linear-gradient(180deg, white, skyblue, white);
     border-right: 10px solid rgb(155, 187, 236);
     border-top-right-radius: 200%;
     border-bottom-right-radius: 1000%;
