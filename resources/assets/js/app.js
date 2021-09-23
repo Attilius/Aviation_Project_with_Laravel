5384906fa5dc0d0cdcc14ca8c4ade6685aa0f434
Attilius <leo20@freemail.hu>
2021-09-23 21:31:07 +0200

    I imported BootstrapVue and IconsPlugin

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,18 @@
 
  require('./bootstrap');
 
- window.Vue = require('vue');
+ //window.Vue = require('vue');
+ import Vue from 'vue'
+ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
  
  /**
   * Next, we will create a fresh Vue application instance and attach it to
