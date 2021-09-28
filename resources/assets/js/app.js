f476cb3fc7c750716b9491c66cc5f866ffef39f3
Attilius <leo20@freemail.hu>
2021-09-28 11:05:00 +0200

    I imported fontawesome in this project

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,18 +14,25 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlaneDeparture, faPlaneArrival } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlaneDeparture, faPlaneArrival);
+
+Vue.config.productionTip = false;
 
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
  
  /**
   * Next, we will create a fresh Vue application instance and attach it to
   * the page. Then, you may begin adding components to this application
   * or customize the JavaScript scaffolding to fit your unique needs.
   */
- 
+ Vue.component('font-awesome-icon', FontAwesomeIcon);
  Vue.component('Welcome', require('./components/Welcome.vue').default);
  
  const app = new Vue({
