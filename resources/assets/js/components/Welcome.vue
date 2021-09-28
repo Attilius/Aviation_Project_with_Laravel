4ed3a23a246b8a01cc5bc4b252268f6f0c546b13
Attilius <leo20@freemail.hu>
2021-09-28 11:07:51 +0200

    I added some new class attributes for welcome component and I set form select

--- resources/assets/js/components/Welcome.vue
+++ resources/assets/js/components/Welcome.vue
@@ -4,23 +4,67 @@
             <b-col cols="12" md="5" class="under-left">
                 <div class="left">
                     <div class="cover-bg">
-                    <b-img :src="'images/A320.png'" fluid alt="Plane"></b-img>
-                    <h2 class="mobile-title-top">Make your trip an <b>experience</b></h2>
-                    <h1 class="mobile-title-bottom">fly with us</h1>
+                        <b-img
+                            :src="'images/A320.png'"
+                            fluid
+                            alt="Plane"
+                        ></b-img>
+                        <h2 class="mobile-title-top">
+                            Make your trip an <b>experience</b>
+                        </h2>
+                        <h1 class="mobile-title-bottom">fly with us</h1>
                     </div>
                 </div>
             </b-col>
             <b-col cols="12" md="7" class="right">
                 <b-container class="open-page-content">
-                    <h2 class="title-top">Make your trip an <b>experience</b></h2>
+                    <h2 class="title-top">
+                        Make your trip an <b>experience</b>
+                    </h2>
                     <h1 class="title-bottom">fly with us</h1>
                     <b-form>
-                        <label for="from">From</label>
-                        <b-form-select name="from" v-model="selected_from" :options="options_from"><i class="fas fa-plane-departure"></i></b-form-select>
-                        <label for="to">To</label>
-                        <b-form-select name="to" v-model="selected_to" :options="options_to"></b-form-select>
-                        <label for="date">Date</label>
-                        <b-form-input name="date" type="date"></b-form-input>
+                        <div class="input-column">
+                            <label for="from">From</label>
+                            <div class="form-icon">
+                                <div class="icon">
+                                    <font-awesome-icon
+                                        :icon="['fas', 'plane-departure']"
+                                    />
+                                </div>
+                                <b-form-select
+                                    class="select"
+                                    name="from"
+                                    v-model="selected_from"
+                                    :options="options_from"
+                                ></b-form-select>
+                            </div>
+                        </div>
+
+                        <div class="input-column">
+                            <label for="to">To</label>
+                            <div class="form-icon">
+                                <div class="icon">
+                                    <font-awesome-icon
+                                        :icon="['fas', 'plane-arrival']"
+                                    />
+                                </div>
+                                <b-form-select
+                                    class="select"
+                                    name="to"
+                                    v-model="selected_to"
+                                    :options="options_to"
+                                ></b-form-select>
+                            </div>
+                        </div>
+
+                        <div class="input-column">
+                            <label for="date">Date</label>
+                            <b-form-input
+                                class="input"
+                                name="date"
+                                type="date"
+                            ></b-form-input>
+                        </div>
                     </b-form>
                 </b-container>
             </b-col>
@@ -32,32 +76,31 @@
 export default {
     data() {
         return {
-        selected_from: null,
-        options_from: [
-          { value: null, text: 'Please select your location of departure' },
-          { value: 'a', text: 'Budapest' },
-          { value: 'b', text: 'London' },
-          { value: 'c', text: 'Paris' },
-          { value: 'd', text: 'Munich' },
-          { value: 'e', text: 'New York' },
-        ],
-        selected_to: null,
-        options_to: [
-          { value: null, text: 'Please select your destination' },
-          { value: 'a', text: 'Budapest' },
-          { value: 'b', text: 'London' },
-          { value: 'c', text: 'Paris' },
-          { value: 'd', text: 'Munich' },
-          { value: 'e', text: 'New York' },
-        ]
-      }
-    },
-}
+            selected_from: null,
+            options_from: [
+                { value: null, text: "Please select" },
+                { value: "a", text: "Budapest" },
+                { value: "b", text: "London" },
+                { value: "c", text: "Paris" },
+                { value: "d", text: "Munich" },
+                { value: "e", text: "New York" }
+            ],
+            selected_to: null,
+            options_to: [
+                { value: null, text: "Please select" },
+                { value: "a", text: "Budapest" },
+                { value: "b", text: "London" },
+                { value: "c", text: "Paris" },
+                { value: "d", text: "Munich" },
+                { value: "e", text: "New York" }
+            ]
+        };
+    }
+};
 </script>
 
-
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap");
 .wrapper {
     width: 100%;
     height: 100hv;
@@ -93,14 +136,20 @@ export default {
 }
 
 .cover-bg {
-    background: linear-gradient(135deg, white, rgba(255,255,255, .1), rgba(0, 0, 0, .9));
+    background: linear-gradient(
+        135deg,
+        white,
+        rgba(255, 255, 255, 0.1),
+        rgba(0, 0, 0, 0.9)
+    );
     height: 100vh;
     width: 100%;
     border-top-right-radius: 100%;
     border-bottom-right-radius: 500%;
 }
 
-.mobile-title-top, .mobile-title-bottom {
+.mobile-title-top,
+.mobile-title-bottom {
     display: none;
 }
 
@@ -119,7 +168,7 @@ export default {
 .title-top {
     font-size: 3.5rem;
     font-weight: 400;
-    color: rgb(9,55,115);
+    color: rgb(9, 55, 115);
     text-transform: capitalize;
     text-shadow: 2px 2px 5px white;
 }
@@ -127,9 +176,52 @@ export default {
 .title-bottom {
     font-size: 4rem;
     font-weight: 500;
-    color: rgb(9,55,115);
+    color: rgb(9, 55, 115);
     text-transform: uppercase;
     text-shadow: 2px 2px 5px white;
+}
+
+form {
+    display: flex;
+}
+
+.select {
+    width: 150px;
+    border-left: none !important;
+    border-top-left-radius: 0 !important;
+    border-bottom-left-radius: 0 !important;
+}
+
+.select:focus {
+    outline-style: none !important;
+}
+
+.input {
+    width: 190px;
+}
+
+.input-column {
+    display: flex;
+    flex-direction: column;
+}
+
+.icon {
+    width: 38px;
+    height: 38px;
+    background: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid #ced4da;
+    border-left: 1px solid #ced4da;
+    border-bottom: 1px solid #ced4da;
+    border-top-left-radius: 0.25rem;
+    border-bottom-left-radius: 0.25rem;
+}
+
+.form-icon {
+    display: flex;
+    width: 200px;
 }
 
 img {
@@ -139,47 +231,53 @@ img {
     margin-left: 180px;
 }
 
-@media (max-width: 760px){
+@media (max-width: 760px) {
     img {
         margin-left: 0px;
     }
 
     .under-left {
-    height: 60vh;
-    width: 100%;
-    padding-right: 0;
-    background: none;
-    border-right: none;
-    border-top-right-radius: 0%;
-    border-bottom-right-radius: 0%;
+        height: 60vh;
+        width: 100%;
+        padding-right: 0;
+        background: none;
+        border-right: none;
+        border-top-right-radius: 0%;
+        border-bottom-right-radius: 0%;
     }
 
     .left {
-    background: url("../../img/city.jpg");
-    height: 60vh;
-    width: 100%;
-    border-top-right-radius: 0%;
-    border-bottom-right-radius: 0%;
+        background: url("../../img/city.jpg");
+        height: 60vh;
+        width: 100%;
+        border-top-right-radius: 0%;
+        border-bottom-right-radius: 0%;
     }
 
     .cover-bg {
-    background: linear-gradient(135deg, white, rgba(255,255,255, .1), rgba(0, 0, 0, .7)) ;
-    height: 60vh;
-    width: 100%;
-    border-top-right-radius: 0%;
-    border-bottom-right-radius: 0%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+        background: linear-gradient(
+            135deg,
+            white,
+            rgba(255, 255, 255, 0.1),
+            rgba(0, 0, 0, 0.7)
+        );
+        height: 60vh;
+        width: 100%;
+        border-top-right-radius: 0%;
+        border-bottom-right-radius: 0%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
     }
 
-    .mobile-title-top, .mobile-title-bottom {
+    .mobile-title-top,
+    .mobile-title-bottom {
         color: whitesmoke;
         font-weight: 500;
         display: inline-block;
         text-align: center;
-        text-shadow: 1px 1px rgb(9,55,115);
+        text-shadow: 1px 1px rgb(9, 55, 115);
     }
 
     .mobile-title-top {
@@ -191,10 +289,11 @@ img {
     }
 
     .right {
-    padding-right: 0;
+        padding-right: 0;
     }
 
-    .title-top, .title-bottom{
+    .title-top,
+    .title-bottom {
         display: none;
     }
 }
