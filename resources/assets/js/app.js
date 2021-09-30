e2e89c0115e3964543be74e9c674244d1cafef3b
Attilius <leo20@freemail.hu>
2021-09-30 19:44:41 +0200

    I modified code format and I imported a new font awesome icon (copyright)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,24 +1,26 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlaneDeparture, faPlaneArrival } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+    faPlaneDeparture,
+    faPlaneArrival
+} from "@fortawesome/free-solid-svg-icons";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPlaneDeparture, faPlaneArrival);
+library.add(faPlaneDeparture, faPlaneArrival, faCopyright);
 
 Vue.config.productionTip = false;
 
@@ -26,15 +28,15 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
- 
- /**
-  * Next, we will create a fresh Vue application instance and attach it to
-  * the page. Then, you may begin adding components to this application
-  * or customize the JavaScript scaffolding to fit your unique needs.
-  */
- Vue.component('font-awesome-icon', FontAwesomeIcon);
- Vue.component('Home', require('./components/Home.vue').default);
- 
- const app = new Vue({
-     el: '#app',
- });
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component("Home", require("./components/Home.vue").default);
+
+const app = new Vue({
+    el: "#app"
+});
