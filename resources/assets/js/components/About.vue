75296f1b88ea5f0615c4e5b16577b026461a62ce
Attilius <leo20@freemail.hu>
2021-10-01 12:52:46 +0200

    I created About.vue component

---
+++ resources/assets/js/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header />
+        <AboutContent />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import AboutContent from "./AboutContent.vue";
+import Footer from "./Footer.vue";
+
+export default {
+    name: "About",
+
+    components: {
+        Header,
+        AboutContent,
+        Footer
+    }
+}
+</script>
