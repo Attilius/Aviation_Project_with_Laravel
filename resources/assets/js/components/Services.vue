008379f96bf0c52eb27850791f68370da00c818a
Attilius <leo20@freemail.hu>
2021-10-01 12:56:46 +0200

    I created Services.vue component

---
+++ resources/assets/js/components/Services.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header />
+        <ServicesContent />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import ServicesContent from "./ServicesContent.vue";
+import Footer from "./Footer.vue";
+
+export default {
+    name: "Services",
+
+    components: {
+        Header,
+        ServicesContent,
+        Footer
+    }
+}
+</script>
