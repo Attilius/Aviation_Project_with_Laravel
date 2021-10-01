5d53f230d8271e786180aec3f4350eabc026ce9f
Attilius <leo20@freemail.hu>
2021-10-01 12:48:15 +0200

    I set routing of menu items

--- resources/assets/js/components/Header.vue
+++ resources/assets/js/components/Header.vue
@@ -10,9 +10,9 @@
                 <b-navbar-nav class="ml-auto">
                     <ul class="menu">
                         <li><a href="/">Home</a></li>
-                        <li><a href="/">About</a></li>
-                        <li><a href="/">Services</a></li>
-                        <li><a href="/">Contact</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/services">Services</a></li>
+                        <li><a href="/contact">Contact</a></li>
                         <li>
                             <select class="select-lang" name="lang">
                                 <option value="EN">EN</option>
