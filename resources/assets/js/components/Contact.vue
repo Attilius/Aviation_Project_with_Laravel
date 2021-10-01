1c2413f15b34cb41fc1952015bc369bf52558cc6
Attilius <leo20@freemail.hu>
2021-10-01 12:54:54 +0200

    I created Contact.vue component

---
+++ resources/assets/js/components/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header />
+        <ContactContent />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import ContactContent from "./ContactContent.vue";
+import Footer from "./Footer.vue";
+
+export default {
+    name: "Contact",
+
+    components: {
+        Header,
+        ContactContent,
+        Footer
+    }
+}
+</script>
