3f6fe9f676f21ddac0f4e08430c38d7bcfc4e0ac
Attilius <leo20@freemail.hu>
2021-10-02 10:04:37 +0200

    I added and set header for content of about page

--- resources/assets/js/components/AboutContent.vue
+++ resources/assets/js/components/AboutContent.vue
@@ -1,11 +1,100 @@
 <template>
     <div>
-        <h1>About</h1>
+        <div class="content-header">
+            <div class="color-box">
+                <h1>Aviation is our passion</h1>
+                <b-img
+                    class="B737"
+                    :src="'images/B737.png'"
+                    fluid
+                    alt="Plane"
+                ></b-img>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "AboutContent"
-}
+};
 </script>
+
+<style scoped>
+*,
+*::after,
+*::before {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Poppins;
+}
+
+.content-header {
+    height: 40vh;
+    width: 100%;
+    background-color: whitesmoke;
+    margin-bottom: 30px;
+}
+
+.color-box {
+    height: 40vh;
+    width: 100%;
+    background: linear-gradient(180deg, rgb(9, 55, 115), rgb(51, 100, 179));
+    border-bottom-right-radius: 80%;
+    display: flex;
+    justify-content: space-around;
+}
+
+h1 {
+    color: whitesmoke;
+    text-shadow: 2px 2px 5px silver;
+    padding: 40px 50px;
+    display: block;
+    width: 400px;
+    font-size: 3rem;
+}
+
+@media (max-width: 1200px) {
+
+    .content-header, .color-box {
+        height: 20%;
+    }
+
+    .color-box {
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    h1 {
+        padding: 20px;
+        display: inline;
+        text-align: center;
+        width: 100%;
+    }
+
+    .B737 {
+        width: 90%;
+        margin: 0 auto;
+    }
+}
+
+@media (max-width: 900px) {
+    h1 {
+        font-size: 2.5rem;
+    }
+}
+
+@media (max-width: 750px) {
+    h1 {
+        font-size: 2rem;
+    }
+}
+
+@media (max-width: 576px) {
+    h1 {
+        font-size: 1rem;
+    }
+}
+
+</style>
