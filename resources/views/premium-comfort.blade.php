bc45f164c7d89f4c081d91474602f91a738960c7
Attilius <leo20@freemail.hu>
2021-10-15 11:12:02 +0200

    I created a new view template called premium-comfort

---
+++ resources/views/premium-comfort.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.premium-comfort')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+    <Premium_comfort></Premium_comfort>
+@endsection
+
+@section('scripts')
+    
+@endsection
