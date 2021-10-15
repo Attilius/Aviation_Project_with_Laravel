dec7a32012db07e171896adf1c4cf7f079d14312
Attilius <leo20@freemail.hu>
2021-10-15 11:08:35 +0200

    I created a new component called Premium_comfort.vue.

---
+++ resources/assets/js/components/Premium_comfort.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header />
+        <Content />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Content from "./PremiumComfortContent.vue"
+import Footer from "./Footer.vue";
+export default {
+    name: "Premium_comfort",
+    components: {
+        Header,
+        Content,
+        Footer
+    }
+};
+</script>
+
