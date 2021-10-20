ea33bf38345d1580f7af7b757c18303abd506300
Attilius <leo20@freemail.hu>
2021-10-20 19:29:43 +0200

    I added resources/views/travel-insurance.blade.php

---
+++ resources/views/travel-insurance.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+    <Travel_insurance></Travel_insurance>
+@endsection
+
+@section('scripts')
+    
+@endsection
