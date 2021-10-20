f8bba3efce63dfc9d5e3ba8cf6c5b29f95e3cc0b
Attilius <leo20@freemail.hu>
2021-10-20 19:27:49 +0200

    I added resources/views/luggage-insurance.blade.php

---
+++ resources/views/luggage-insurance.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+    <Luggage_insurance></Luggage_insurance>
+@endsection
+
+@section('scripts')
+    
+@endsection
