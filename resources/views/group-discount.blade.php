ce9b16443e2d213cf73940889e704214e8e4a090
Attilius <leo20@freemail.hu>
2021-10-20 19:26:09 +0200

    I added resources/views/group-discount.blade.php

---
+++ resources/views/group-discount.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+    <Group_discount></Group_discount>
+@endsection
+
+@section('scripts')
+    
+@endsection
