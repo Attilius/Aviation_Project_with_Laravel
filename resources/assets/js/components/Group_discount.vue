2da6957993189aa6ccd5cfc5d4e0679e15cafce3
Attilius <leo20@freemail.hu>
2021-10-20 19:33:15 +0200

    I added resources/assets/js/components/Group_discount.vue

---
+++ resources/assets/js/components/Group_discount.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <Header class="header" />
+        <div class="wrapper">
+            <div class="skin">
+                <AnimatedHeader :titles="titles" :images="images" />
+                <ServiceMainContent
+                    :titles="titles"
+                    :text="text"
+                    :important_text="important_text"
+                    :items="items"
+                    :fields="fields"
+                />
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import AnimatedHeader from "./AnimatedHeader.vue";
+import Footer from "./Footer.vue";
+export default {
+    name: "Premium_comfort",
+    components: {
+        Header,
+        AnimatedHeader,
+        Footer
+    },
+
+    data() {
+        return {
+            titles: {
+                first: "Group discount",
+                second: "Share with friends"
+            },
+            images: {
+                first: {
+                    backgroundImage: `url(${require("../../img/group-1.jpg")})`
+                },
+                second: {
+                    backgroundImage: `url(${require("../../img/group-2.jpg")})`
+                },
+                third: {
+                    backgroundImage: `url(${require("../../img/group-3.jpg")})`
+                },
+                fourth: {
+                    backgroundImage: `url(${require("../../img/group-4.jpg")})`
+                },
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.header {
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 0;
+        z-index: 1;
+    }
+
+.wrapper {
+    background: url("../../img/airport-bg.jpg");
+    background-size: cover;
+    height: 150vh;
+    width: 100%;
+}
+
+.skin {
+    height: 150vh;
+    width: 100%;
+    background: rgba(9, 55, 115, 0.8);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+</style>
