a482c24684898a108760db30e0d1ef40ab088b01
Attilius <leo20@freemail.hu>
2021-10-20 19:25:07 +0200

    I added resources/views/booking-cancellation.blade.php

---
+++ resources/views/booking-cancellation.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+    <Booking_cancellation></Booking_cancellation>
+@endsection
+
+@section('scripts')
+    
+@endsection
