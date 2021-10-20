94b508d880dd714586b6b12e00881978a390ea41
Attilius <leo20@freemail.hu>
2021-10-20 19:32:19 +0200

    I added resources/assets/js/components/Private_jet_rent.vue

---
+++ resources/assets/js/components/Private_jet_rent.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <Header class="header" />
+        <div class="wrapper">
+            <div class="skin">
+                <AnimatedHeader :titles="titles" :images="images" />
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import AnimatedHeader from "./AnimatedHeader.vue";
+import Footer from "./Footer.vue";
+export default {
+    name: "Premium_comfort",
+    components: {
+        Header,
+        AnimatedHeader,
+        Footer
+    },
+
+    data() {
+        return {
+            titles: {
+                first: "Private jet rent",
+                second: "Get private service"
+            },
+            images: {
+                first: {
+                    backgroundImage: `url(${require("../../img/private-1.jpg")})`
+                },
+                second: {
+                    backgroundImage: `url(${require("../../img/private-2.jpg")})`
+                },
+                third: {
+                    backgroundImage: `url(${require("../../img/private-3.jpg")})`
+                },
+                fourth: {
+                    backgroundImage: `url(${require("../../img/private-4.jpg")})`
+                },
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.header {
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 0;
+        z-index: 1;
+    }
+
+.wrapper {
+    background: url("../../img/airport-bg.jpg");
+    background-size: cover;
+    height: 150vh;
+    width: 100%;
+}
+
+.skin {
+    height: 150vh;
+    width: 100%;
+    background: rgba(9, 55, 115, 0.8);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+</style>
