a36f5fbc1e2cc582197c7519e7dee4b3c76970db
Attilius <leo20@freemail.hu>
2021-10-20 19:28:40 +0200

    I added resources/views/private-jet-rent.blade.php

---
+++ resources/views/private-jet-rent.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('style')
+    
+@endsection
+
+@section('content')
+    <Private_jet_rent></Private_jet_rent>
+@endsection
+
+@section('scripts')
+    
+@endsection
