be5a0aa539eeec3dc0237114aaed09c56d3a0d68
Attilius <leo20@freemail.hu>
2021-10-29 21:41:01 +0200

    I added new component called CancelledContent.vue

---
+++ resources/assets/js/components/CancelledContent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="text-content">
+        <h2>{{ titles.first }}</h2>
+        <article>{{ text }}</article>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CancelledContent",
+    props: ['titles', 'text'],
+}
+</script>
+
+<style scoped>
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.text-content {
+    background: whitesmoke;
+    width: 80%;
+    height: 300px;
+    margin: 0 auto;
+    position: relative;
+    top: 90px;
+}
+
+h2::after {
+    content: "";
+    position: absolute;
+    background-color: rebeccapurple;
+    width: 20%;
+    height: 7px;
+    bottom: -10px;
+    left: 30px;
+    margin-bottom: 20px;
+}
+
+h2 {
+    padding: 15px 20px 25px 30px;
+    position: relative;
+    margin-top: 20px;
+    color: rgb(9, 55, 115);
+    text-transform: capitalize;
+    text-shadow: 2px 2px 5px silver;
+    width: 30%;
+}
+
+article {
+    padding: 25px;
+    margin: 5px;
+}
+
+</style>
