16ba9f77aad348dbd3a44860229917b660546f07
Attilius <leo20@freemail.hu>
2021-10-30 20:54:18 +0200

    I set display of mobile view to animated-header

--- resources/assets/js/components/Booking_cancellation.vue
+++ resources/assets/js/components/Booking_cancellation.vue
@@ -3,7 +3,7 @@
         <Header class="header" />
         <div class="wrapper">
             <div class="skin">
-                <AnimatedHeader :titles="titles" :images="images" />
+                <AnimatedHeader class="animated-header" :titles="titles" :images="images" />
                 <ServicesMainContent :titles="titles" :text="text" />
             </div>
         </div>
@@ -74,4 +74,11 @@ export default {
     flex-direction: column;
     justify-content: space-around;
 }
+
+@media(max-width: 768px) {
+    .animated-header {
+        display: none;
+    }
+}
+
 </style>
