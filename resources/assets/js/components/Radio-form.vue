acaf40d90cdc9fcc6b7e20f3b447160f3afeeaad
Attilius <leo20@freemail.hu>
2021-11-01 10:21:54 +0100

    Modified public/js/app.js

--- resources/assets/js/components/Radio-form.vue
+++ resources/assets/js/components/Radio-form.vue
@@ -1,49 +1,50 @@
 <template>
-  <div>
+    <div>
+        <b-form-group label="Stacked radios" v-slot="{ ariaDescribedby }">
+            <b-form-radio-group
+                v-model="selected"
+                :options="options"
+                :aria-describedby="ariaDescribedby"
+                name="radios-stacked"
+                stacked
+            ></b-form-radio-group>
+        </b-form-group>
 
-    <b-form-group label="Stacked radios" v-slot="{ ariaDescribedby }">
-      <b-form-radio-group
-        v-model="selected"
-        :options="options"
-        :aria-describedby="ariaDescribedby"
-        name="radios-stacked"
-        stacked
-      ></b-form-radio-group>
-    </b-form-group>
+        <div class="mt-3">
+            Selected: You get <strong>{{ selected }} % </strong> discount from
+            the full price of ticket.
+        </div>
 
-    <div class="mt-3">Selected: You get <strong>{{ selected }} % </strong> discount from the full price of ticket.</div>
-
-    <b-form @submit="onSubmit">
+        <b-form @submit="onSubmit">
             <b-button id="submit-btn" type="submit" variant="primary"
                 >Submit</b-button
             >
         </b-form>
-  </div>
+    </div>
 </template>
 
 <script>
-  export default {
-      name: "RadioForm",
+export default {
+    name: "RadioForm",
     data() {
-      return {
-        selected: "",
-        form: [],
-      
-        options: [
-          { text: 'Small group (10-15 person)', value: 10 },
-          { text: 'Medium group (15-20 person)', value: 15 },
-          { text: 'Large group (20-25 person)', value: 20 },
-          { text: 'X-Large group (more than 25 person)', value: 25 },
-        ]
-      }
+        return {
+            selected: "",
+            form: [],
+
+            options: [
+                { text: "Small group (10-15 person)", value: 10 },
+                { text: "Medium group (15-20 person)", value: 15 },
+                { text: "Large group (20-25 person)", value: 20 },
+                { text: "X-Large group (more than 25 person)", value: 25 }
+            ]
+        };
     },
 
     methods: {
         onSubmit(event) {
             event.preventDefault();
-                alert("Empty form");
-        },
+            alert("Empty form");
+        }
     }
-
-  }
+};
 </script>
