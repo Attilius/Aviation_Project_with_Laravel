d4a093fbbc9f2d45759b8c7ddf0383fd5303e912
Attilius <leo20@freemail.hu>
2021-11-01 17:56:20 +0100

    I added change event for every radio button next I added and set onChange method, finally I modifed onSubmit method

--- resources/assets/js/components/Radio-form.vue
+++ resources/assets/js/components/Radio-form.vue
@@ -6,6 +6,7 @@
                 v-model="selected"
                 :aria-describedby="ariaDescribedby"
                 name="some-radios"
+                @change="onChange"
                 value="5"
                 >Family (5-9 person) - 5%</b-form-radio
             >
@@ -13,6 +14,7 @@
                 v-model="selected"
                 :aria-describedby="ariaDescribedby"
                 name="some-radios"
+                @change="onChange"
                 value="10"
                 >Small group (10-14 person) - 10%</b-form-radio
             >
@@ -20,6 +22,7 @@
                 v-model="selected"
                 :aria-describedby="ariaDescribedby"
                 name="some-radios"
+                @change="onChange"
                 value="15"
                 >Medium group (15-20 person) - 15%</b-form-radio
             >
@@ -27,6 +30,7 @@
                 v-model="selected"
                 :aria-describedby="ariaDescribedby"
                 name="some-radios"
+                @change="onChange"
                 value="20"
                 >Large group (21-25 person) - 20%</b-form-radio
             >
@@ -34,12 +38,13 @@
                 v-model="selected"
                 :aria-describedby="ariaDescribedby"
                 name="some-radios"
+                @change="onChange"
                 value="25"
                 >X-Large group (more than 25 person) - 25%</b-form-radio
             >
         </b-form-group>
 
-        <div class="mt-3">
+        <div class="mt-3" id="selected-message">
             You get <strong>{{ selected }} % </strong> discount from
             the full price of ticket.
         </div>
@@ -65,7 +70,15 @@ export default {
     methods: {
         onSubmit(event) {
             event.preventDefault();
-            alert("Empty form");
+            alert(JSON.stringify(this.form));
+            this.form = [];
+        },
+
+        onChange(event) {
+            document.getElementById("selected-message").style.display = "inline";
+            
+            if (this.form.length === 0) this.form.push(event); 
+            else this.selected = "";
         }
     }
 };
@@ -80,4 +93,9 @@ export default {
     border: none !important;
     box-shadow: none !important;
 }
+
+#selected-message {
+    display: none;
+}
+
 </style>
