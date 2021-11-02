1256581e4cfd3551aeb4f995e0dd856770429943
Attilius <leo20@freemail.hu>
2021-11-02 13:13:52 +0100

    I set datas of props in b-form-radio tag and I used v-for directive finally I created a new method called addText. This method push the text which come in props in to the form array

--- resources/assets/js/components/Radio-form.vue
+++ resources/assets/js/components/Radio-form.vue
@@ -1,54 +1,23 @@
 <template>
     <div>
         <b-form-group v-slot="{ ariaDescribedby }">
-            <h5 class="radio-form-label">Selecting discount</h5>
+            <h5 class="radio-form-label">Selecting {{ label }}</h5>
             <b-form-radio
                 v-model="selected"
                 :aria-describedby="ariaDescribedby"
                 name="some-radios"
+                v-for="radio_data in radio_datas"
+                :key="radio_data.text"
                 @change="onChange"
-                value="5"
-                >Family (5-9 person) - 5%</b-form-radio
-            >
-            <b-form-radio
-                v-model="selected"
-                :aria-describedby="ariaDescribedby"
-                name="some-radios"
-                @change="onChange"
-                value="10"
-                >Small group (10-14 person) - 10%</b-form-radio
-            >
-            <b-form-radio
-                v-model="selected"
-                :aria-describedby="ariaDescribedby"
-                name="some-radios"
-                @change="onChange"
-                value="15"
-                >Medium group (15-20 person) - 15%</b-form-radio
-            >
-            <b-form-radio
-                v-model="selected"
-                :aria-describedby="ariaDescribedby"
-                name="some-radios"
-                @change="onChange"
-                value="20"
-                >Large group (21-25 person) - 20%</b-form-radio
-            >
-            <b-form-radio
-                v-model="selected"
-                :aria-describedby="ariaDescribedby"
-                name="some-radios"
-                @change="onChange"
-                value="25"
-                >X-Large group (more than 25 person) - 25%</b-form-radio
+                :value="radio_data.value"
+                >{{ radio_data.text }}</b-form-radio
             >
         </b-form-group>
 
-        <p id="welcome-message">You don't selected any discount yet*</p>
+        <p id="welcome-message">You don't selected any {{ label }} yet*</p>
 
         <p id="selected-message">
-            You get <strong>{{ selected }} % </strong> discount from
-            the full price of ticket.
+            {{ message[0] }} <strong>{{ selected }}</strong> {{ message[1] }}
         </p>
 
         <b-form @submit="onSubmit">
@@ -62,10 +31,11 @@
 <script>
 export default {
     name: "RadioForm",
+    props: ["radio_datas", "label", "message"],
     data() {
         return {
             selected: "",
-            form: [],
+            form: []
         };
     },
 
@@ -78,22 +48,52 @@ export default {
             this.viewMessage();
         },
 
+        addText(event) {
+            switch (event) {
+                case 5:
+                    this.form.push(this.radio_datas[0].text);
+                    break;
+                case 10:
+                    this.form.push(this.radio_datas[1].text);
+                    break;
+                case 15:
+                    this.form.push(this.radio_datas[2].text);
+                    break;
+                case 20:
+                    this.form.push(this.radio_datas[3].text);
+                    break;
+                case 25:
+                    this.form.push(this.radio_datas[4].text);
+                    break;
+
+                default:
+                    break;
+            }
+        },
+
         onChange(event) {
             this.viewMessage();
-            if (this.form.length === 0) this.form.push(event); 
-            else {
-               this.form = [];
-               this.form.push(event);
+            if (this.form.length === 0) {
+                this.addText(event);
+                this.form.push(event);
+            } else {
+                this.form = [];
+                this.addText(event);
+                this.form.push(event);
             }
         },
 
         viewMessage() {
             if (this.selected === "") {
-                document.getElementById("selected-message").style.display = "none";
-                document.getElementById("welcome-message").style.display = "inline";
+                document.getElementById("selected-message").style.display =
+                    "none";
+                document.getElementById("welcome-message").style.display =
+                    "inline";
             } else {
-                document.getElementById("selected-message").style.display = "inline";
-                document.getElementById("welcome-message").style.display = "none";
+                document.getElementById("selected-message").style.display =
+                    "inline";
+                document.getElementById("welcome-message").style.display =
+                    "none";
             }
         }
     }
@@ -118,5 +118,4 @@ export default {
     border: 1px solid #292b2c;
     padding: 10px;
 }
-
 </style>
