afcc71e88430fcc019a818a956b285186f5e4400
Attilius <leo20@freemail.hu>
2021-11-07 15:44:23 +0100

    I added a form and three input field and submit btn. Next I added two new pair of key-value for radio_datas. Finally I set css

--- resources/assets/js/components/Private_jet_rent.vue
+++ resources/assets/js/components/Private_jet_rent.vue
@@ -5,6 +5,50 @@
             <div class="skin">
                 <AnimatedHeader class="animated-header" :titles="titles" :images="images" />
                 <ServicesMainContent :titles="titles" :text="text" :radio_datas="radio_datas" :label="label" :message="message" />
+                <div class="input-box">
+            <div class="input-column">
+                <label for="from">From</label>
+                <div class="form-icon">
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fas', 'plane-departure']" />
+                    </div>
+                    <b-form-input
+                    id="from"
+                        class="input"
+                        name="from"
+                        type="text"
+                    ></b-form-input>
+                </div>
+            </div>
+
+            <div class="input-column">
+                <label for="to">To</label>
+                <div class="form-icon">
+                    <div class="icon">
+                        <font-awesome-icon :icon="['fas', 'plane-arrival']" />
+                    </div>
+                    <b-form-input
+                    id="to"
+                        class="input"
+                        name="to"
+                        type="text"
+                    ></b-form-input>
+                </div>
+            </div>
+
+            <div class="input-column">
+                <label for="date">Date of away</label>
+                <b-form-input
+                    class="input-date"
+                    name="date"
+                    type="date"
+                ></b-form-input>
+            </div>
+            <b-form @submit="onSubmit">
+                <button type="submit" id="submit-btn">Send</button>
+            </b-form>
+            
+        </div>
             </div>
         </div>
         <Footer />
@@ -52,51 +96,64 @@ export default {
                 {
                     img: "/images/eclipse-500.jpg",
                     text: "Eclipse 500",
+                    speed: 685,
+                    range_distance: 2084,
                     staff: "3 person",
                     passenger_capacity: "4 people",
-                    price: "1500 €/h",
+                    price: 1500,
                     value: 100
                 },
                 {
                     img: "/images/embraer-phenom-100.png",
                     text: "Embraer Phenom 100",
+                    speed: 750,
+                    range_distance: 2182,
                     staff: "1 person",
                     passenger_capacity: "4-7 people",
-                    price: "1800 €/h",
+                    price: 1800,
                     value: 200
                 },
                 {
                     img: "/images/cessna-citation-latitude.jpg",
                     text: "Cessna Citation Latitude",
+                    speed: 826,
+                    range_distance: 5332,
                     staff: "2 person",
                     passenger_capacity: "7-9 people",
-                    price: "2100 €/h",
+                    price: 2400,
                     value: 300
                 },
                 {
                     img: "/images/bombardier-challenger-605.jpg",
                     text: "Bombardier Challenger 605",
+                    speed: 870,
+                    range_distance: 7400,
                     staff: "2 person",
                     passenger_capacity: "9-12 people",
-                    price: "2500 €/h",
+                    price: 2900,
                     value: 400
                 },
                 {
                     img: "/images/dassault-falcon-7X.jpg",
                     text: "Dassault Falcon 7X",
+                    speed: 900,
+                    range_distance: 11019,
                     staff: "3 person",
                     passenger_capacity: "12-16 people",
-                    price: "2800 €/h",
+                    price: 3500,
                     value: 500
                 }
             ],
 
             label: "from our jet offer",
 
-            message: []
+            message: [],
+
+            from: "",
+            to: "",
         };
-    }
-};
+    },
+}
 </script>
 
 <style scoped>
@@ -110,17 +167,91 @@ export default {
 .wrapper {
     background: url("../../img/airport-bg.jpg");
     background-size: cover;
-    height: 130vh;
+    height: 150vh;
     width: 100%;
 }
 
 .skin {
-    height: 130vh;
+    height: 150vh;
     width: 100%;
     background: rgba(9, 55, 115, 0.8);
     display: flex;
     flex-direction: column;
     justify-content: space-around;
+}
+
+.animated-header {
+    margin-bottom: 350px;
+}
+
+.input-box {
+    display: flex;
+    width: 80%;
+    margin: 0 auto;
+    padding: 30px;
+}
+
+.input {
+    width: 150px;
+    border-left: none !important;
+    border-top-left-radius: 0 !important;
+    border-bottom-left-radius: 0 !important;
+    box-shadow: none;
+}
+
+.input:focus {
+    box-shadow: none !important;
+}
+
+.input-date {
+    border-radius: 0.25rem;
+    box-shadow: 2px 2px 5px black;
+}
+
+.input-date:focus {
+    box-shadow: 2px 2px 5px black !important;
+}
+
+.input-column {
+    margin-right: 10px;
+}
+
+.form-icon {
+    display: flex;
+    width: 180px;
+    box-shadow: 2px 2px 5px black;
+}
+
+label {
+    color: whitesmoke;
+    text-shadow: none;
+}
+
+#submit-btn {
+    background: RebeccaPurple;
+    width: 80px;
+    height: 40px;
+    box-shadow: 2px 2px 5px black;
+    border: none;
+    border-radius: 0.25rem;
+    margin-top: 40px;
+    color: whitesmoke;
+    font-size: 1rem;
+    transition: all 0.3s ease;
+}
+
+#submit-btn:active {
+    box-shadow: none !important;
+    background-color: rebeccapurple !important;
+    color: whitesmoke !important;
+}
+
+#submit-btn:hover {
+    color: rebeccapurple;
+    background: whitesmoke;
+    border: 1px solid rebeccapurple;
+    box-shadow: none;
+    transition: 0.4s ease-in-out 0s;
 }
 
 @media(max-width: 768px) {
