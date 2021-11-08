11071e4a7a014a9cd4a2c1d2c3f2771cb3a126f8
Attilius <leo20@freemail.hu>
2021-11-08 11:15:13 +0100

    I reorganised inputs and his css settings from Private_jet_rent.vue component

--- resources/assets/js/components/ServicesMainContent.vue
+++ resources/assets/js/components/ServicesMainContent.vue
@@ -2,6 +2,55 @@
     <div class="text-content">
         <h2>{{ titles.first }}</h2>
         <article>{{ text }}</article>
+
+        <div class="input-box">
+                    <div class="input-column">
+                        <label for="from">From</label>
+                        <div class="form-icon">
+                            <div class="icon">
+                                <font-awesome-icon
+                                    :icon="['fas', 'plane-departure']"
+                                />
+                            </div>
+                            <b-form-input
+                                id="from"
+                                class="input"
+                                name="from"
+                                type="text"
+                            ></b-form-input>
+                        </div>
+                    </div>
+
+                    <div class="input-column">
+                        <label for="to">To</label>
+                        <div class="form-icon">
+                            <div class="icon">
+                                <font-awesome-icon
+                                    :icon="['fas', 'plane-arrival']"
+                                />
+                            </div>
+                            <b-form-input
+                                id="to"
+                                class="input"
+                                name="to"
+                                type="text"
+                            ></b-form-input>
+                        </div>
+                    </div>
+
+                    <div class="input-column">
+                        <label for="date">Date of away</label>
+                        <b-form-input
+                            class="input-date"
+                            name="date"
+                            type="date"
+                        ></b-form-input>
+                    </div>
+                    <b-form @submit="onSubmit">
+                        <button type="submit" id="submit-btn">Send</button>
+                    </b-form>
+                </div>
+
         <RadioForm
             class="radio-form"
             :radio_datas="radio_datas"
@@ -16,9 +65,36 @@
 import RadioForm from "./Radio-form.vue";
 export default {
     name: "ServicesMainContent",
-    props: ["titles", "text", "radio_datas", "label", "message", "distance_"],
+    props: ["titles", "text", "radio_datas", "label", "message"],
     components: {
         RadioForm
+    },
+
+    data() {
+        return {
+            from: "",
+            to: "",
+            distance_: ""
+        }
+    },
+
+    methods: {
+        onSubmit(event) {
+            event.preventDefault();
+            this.from = document.getElementById("from").value;
+            this.to = document.getElementById("to").value;
+            this.fetchData();
+            document.getElementById("from").value = "";
+            document.getElementById("to").value = "";
+        },
+
+        fetchData() {
+            fetch(`/api/private-jet-rent?departure_location=${this.from}&destination=${this.to}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.distance_ = data.distance;
+                });
+        }
     }
 }
 </script>
@@ -39,7 +115,7 @@ export default {
     margin: 0 auto;
     position: relative;
     top: 105px;
-    margin-bottom: 100px;
+    margin-bottom: 250px;
 }
 
 h2::after {
@@ -74,6 +150,78 @@ article {
 
 .radio-form {
     padding: 30px;
+}
+
+.input-box {
+    display: flex;
+    width: 95%;
+    margin: 0 auto;
+    padding-top: 30px;
+}
+
+.input {
+    width: 150px;
+    border-left: none !important;
+    border-top-left-radius: 0 !important;
+    border-bottom-left-radius: 0 !important;
+    box-shadow: none;
+    text-align: center;
+}
+
+.input:focus {
+    box-shadow: none !important;
+}
+
+.input-date {
+    border-radius: 0.25rem;
+    box-shadow: 2px 2px 5px black;
+    text-align: center;
+}
+
+.input-date:focus {
+    box-shadow: 2px 2px 5px black !important;
+}
+
+.input-column {
+    margin-right: 10px;
+}
+
+.form-icon {
+    display: flex;
+    width: 180px;
+    box-shadow: 2px 2px 5px black;
+}
+
+label {
+    color: whitesmoke;
+    text-shadow: none;
+}
+
+#submit-btn {
+    background: RebeccaPurple;
+    width: 80px;
+    height: 40px;
+    box-shadow: 2px 2px 5px black;
+    border: none;
+    border-radius: 0.25rem;
+    margin-top: 22px;
+    color: whitesmoke;
+    font-size: 1rem;
+    transition: all 0.3s ease;
+}
+
+#submit-btn:active {
+    box-shadow: none !important;
+    background-color: rebeccapurple !important;
+    color: whitesmoke !important;
+}
+
+#submit-btn:hover {
+    color: rebeccapurple;
+    background: whitesmoke;
+    border: 1px solid rebeccapurple;
+    box-shadow: none;
+    transition: 0.4s ease-in-out 0s;
 }
 
 @media (max-width: 1350px) {
