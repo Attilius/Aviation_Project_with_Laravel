5f01dbd547dfe5a463098438f23c0fc4e66b19f3
Attilius <leo20@freemail.hu>
2021-11-26 20:26:13 +0100

    I imported a new fontawesome icon

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -35,7 +35,7 @@ import {
     faClock,
     faExternalLinkAlt
 } from "@fortawesome/free-solid-svg-icons";
-import { faCopyright, faComments } from "@fortawesome/free-regular-svg-icons";
+import { faCopyright, faComments, faEdit } from "@fortawesome/free-regular-svg-icons";
 import { faAvianex } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import Home from "../js/components/Home.vue";
@@ -78,7 +78,8 @@ library.add(
     faEnvelope,
     faMapMarkerAlt,
     faClock,
-    faExternalLinkAlt
+    faExternalLinkAlt,
+    faEdit
 );
 
 Vue.config.productionTip = false;
