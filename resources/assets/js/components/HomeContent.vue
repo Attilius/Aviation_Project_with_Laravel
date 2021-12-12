a339eed33303505e4e783e628624ceb698a18d6c
Attilius <leo20@freemail.hu>
2021-12-12 22:25:23 +0100

    I added content for montreal card

--- resources/assets/js/components/HomeContent.vue
+++ resources/assets/js/components/HomeContent.vue
@@ -180,7 +180,11 @@
                 </div>
             </div>
             <div id="montreal" class="card">
-
+                <h4><span>from</span>100 €*</h4>
+                <div class="skin-cover">
+                    <h6>canada</h6>
+                    <h3>montreal</h3>
+                </div>
             </div>
             <div id="munich" class="card">
 
