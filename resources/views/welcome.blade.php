04d5b15cb798d38f761e0667096ef07a59c9749c
Attilius <leo20@freemail.hu>
2022-01-18 21:41:08 +0100

    I added welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Lorem Airlines</title>
+
+    </head>
+    
+    <body>
+
+        <div id="app"></div>
+
+        <script>
+            var BASE_URL = '{{ URL::to('/') }}';
+        </script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
