da53af6ad59553a0a3589d2621655c698a73d4e6
Attilius <leo20@freemail.hu>
2022-01-19 19:59:08 +0100

    I modified app.php

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        // Database in MongoDB Provider...
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
     ],
 
     /*
