af92c644e168ad0b08e24634f6747544e783a93f
Attilius <leo20@freemail.hu>
2022-01-21 20:52:21 +0100

    I added Header.vue component

---
+++ resources/assets/js/components/travel_guide/city_components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="top">
+            <VideoBackground :city="city" />
+            <HeaderFront :city="city" :app="app" />
+        </div>
+    </div>
+</template>
+
+<script>
+import VideoBackground from "../../../components/travel_guide/city_components/VideoBackground.vue"
+import HeaderFront from "../../../components/travel_guide/city_components/HeaderFront.vue"
+export default {
+    name: "Header",
+    props: ["city", "app"],
+
+    components: {
+        VideoBackground,
+        HeaderFront,
+    }
+}
+</script>
+
+<style scoped>
+.top {
+    color: whitesmoke;
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    top: 4rem;
+}
+</style>
