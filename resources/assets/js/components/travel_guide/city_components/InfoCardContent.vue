ac363a86b7e7dab38da27ffa68379342b84037df
Attilius <leo20@freemail.hu>
2022-01-21 20:57:20 +0100

    I added InfoCardContent.vue componenent

---
+++ resources/assets/js/components/travel_guide/city_components/InfoCardContent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card-content">
+        <div
+            class="info-box-content"
+            v-if="infoItem == `Getting around in ${city.name}`"
+        >
+            <iframe
+                :src="city.map"
+                width="700"
+                height="450"
+                style="border:0;"
+                allowfullscreen=""
+                loading="lazy"
+            ></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InfoCardContent",
+    props: ["infoItem", "city"],
+}
+</script>
+
+<style scoped>
+.card-content{
+    margin:auto 0;
+}
+
+.info-box-content {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
