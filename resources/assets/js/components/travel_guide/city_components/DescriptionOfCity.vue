3ad5f246c0e60099fceb90d9be16a2d1c0df4e29
Attilius <leo20@freemail.hu>
2022-01-21 20:50:08 +0100

    I added DescriptionOfCity.vue component

---
+++ resources/assets/js/components/travel_guide/city_components/DescriptionOfCity.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <p id="quotes">
+            <i>
+                <strong>{{ city.quotes }}</strong>
+                {{ city.author }}
+            </i>
+        </p>
+        <article>
+            {{ city.article }}
+            <span class="read-more-text">
+                {{ city.article_continue }}
+            </span>
+        </article>
+        <span class="read-more-btn" @click="readMore">Read more...</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DescriptionOfCity",
+    props: ["city"],
+
+    methods: {
+        readMore(e) {
+            const current = e.target;
+            const isReadMoreBtn = current.className.includes("read-more-btn");
+            const currentText = e.target.parentNode.querySelector(
+                ".read-more-text"
+            );
+
+            if (!isReadMoreBtn) return;
+
+            currentText.classList.toggle("read-more-text--show");
+            current.textContent = current.textContent.includes("Read more")
+                ? "Read less..."
+                : "Read more...";
+        },
+    }
+};
+</script>
+
+<style scoped>
+#quotes {
+    padding: 5px 10px 10px 0;
+}
+
+.read-more-btn {
+    color: #0984e3;
+    cursor: pointer;
+}
+
+.read-more-text {
+    display: none;
+}
+
+.read-more-text--show {
+    display: inline;
+}
+</style>
