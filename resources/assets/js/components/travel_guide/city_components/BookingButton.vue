aa9b0981eab25689fe8b5c2829c6066691962736
Attilius <leo20@freemail.hu>
2022-01-21 20:49:30 +0100

    I added BookingButton.vue component

---
+++ resources/assets/js/components/travel_guide/city_components/BookingButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="box">
+        <button class="btn">
+            <i id="rotate-90deg" class="material-icons">flight</i>
+            {{ city.name }} ({{ city.city_code }})
+            <i id="rotate-90deg" class="material-icons">expand_less</i>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookingButton",
+    props: ["city"]
+}
+</script>
+
+<style scoped>
+.box {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn {
+    background: rebeccapurple;
+    height: 50px;
+    width: 15%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 50px;
+    border-radius: 0.25rem;
+}
+
+.btn:hover {
+    color: whitesmoke;
+}
+
+#rotate-90deg {
+    transform: rotate(90deg);
+}
+</style>
