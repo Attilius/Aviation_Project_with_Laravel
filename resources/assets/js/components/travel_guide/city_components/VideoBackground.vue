9936740b7987393f31b38b2df57257a109770bff
Attilius <leo20@freemail.hu>
2022-01-22 18:46:21 +0100

    I added VideoBackground.vue component

---
+++ resources/assets/js/components/travel_guide/city_components/VideoBackground.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <video
+            id="video"
+            :src="city.header_video"
+            :poster="city.header_video_poster"
+            autoplay
+            loop
+            playsinline
+            muted
+        ></video>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VideoBackground",
+    props: ["city"],
+}
+</script>
+
+<style scoped>
+#video {
+    object-fit: cover;
+    width: 100%;
+    height: 92vh;
+    top: 0;
+    left: 0;
+}
+</style>
