1de827a579d6b7a49425e2f3be6a27dc887a9ea8
Attilius <leo20@freemail.hu>
2022-01-22 18:48:31 +0100

    I added Register.vue component

---
+++ resources/assets/js/pages/Register.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div class="alert alert-danger" v-if="errors.length">
+            <ul>
+                <li v-for="(error, index) in errors" :key="index">
+                    {{error}}
+                </li>
+            </ul>
+        </div>
+        <b-form @submit.prevent="onSubmit">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" v-model="name">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" v-model="email">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" v-model="password">
+            <input type="submit" value="Sign Up">
+        </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Register',
+    props: ['app'],
+
+    data(){
+        return {
+            name: "",
+            email: "",
+            password: "",
+            errors: []
+        }
+    },
+
+    methods: {
+        onSubmit() {
+            this.errors = [];
+
+            if (!this.name) {
+                this.errors.push("Name is requred!");
+            }
+            if (!this.email) {
+                this.errors.push("Email is requred!");
+            }
+            if (!this.password) {
+                this.errors.push("Password is requred!");
+            }
+
+            if (!this.errors.length) {
+                const data = {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password
+                }
+
+                this.app.req.post('auth/register', data).then(response => {
+                    this.app.user = response.data;
+                    this.$router.push('/');
+                }).catch(error => {
+                    this.errors.push(error.response.data.error);
+                })
+            }
+        }
+    }
+}
+</script>
