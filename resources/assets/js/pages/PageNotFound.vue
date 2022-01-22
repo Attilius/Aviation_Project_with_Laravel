f1e53a2d3ed42d94af88a93f8b33717640c804d0
Attilius <leo20@freemail.hu>
2022-01-22 18:49:03 +0100

    I added PageNotFound.vue component

---
+++ resources/assets/js/pages/PageNotFound.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <Header class="header" :app="app" @onClickLogin="updateLoginPath($event)" />
+        <div class="content">
+            <h1>Page is Not found :(</h1>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+    name: "PageNotFound",
+    props: ['app'],
+
+    components: {
+        Header, 
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+.header {
+    position: -webkit-sticky; /* Safari */
+    position: sticky;
+    top: 0;
+    z-index: 11;
+}
+
+.content {
+    height: 100vh;
+    width: 100%;
+}
+</style>
