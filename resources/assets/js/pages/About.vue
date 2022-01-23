0c4b60d1cfb169e1173a0beb34c9272da5028e4b
Attilius <leo20@freemail.hu>
2022-01-23 19:40:26 +0100

    Modified About.vue component

---
+++ resources/assets/js/pages/About.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <Header class="header" :app="app" @onClickLogin="updateLoginPath($event)" />
+        <AboutContent />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import AboutContent from "../components/AboutContent.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+    name: "About",
+    props: ["app"],
+
+    components: {
+        Header,
+        AboutContent,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+   .header {
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 0;
+        z-index: 11;
+    }
+</style>
