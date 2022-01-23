28ec5466fa8aeb6c734681d553387b0e9dba17c8
Attilius <leo20@freemail.hu>
2022-01-23 19:41:26 +0100

    Modified Contact.vue component

---
+++ resources/assets/js/pages/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <Header class="header" :app="app" @onClickLogin="updateLoginPath($event)" />
+        <ContactContent />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import ContactContent from "../components/ContactContent.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+    name: "Contact",
+    props: ['app'],
+
+    components: {
+        Header,
+        ContactContent,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+   .header {
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 0;
+        z-index: 11;
+    }
+</style>
