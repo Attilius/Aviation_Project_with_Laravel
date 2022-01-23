37eea8516dc906b2df89eb0a5d73d1bcd1466f38
Attilius <leo20@freemail.hu>
2022-01-23 19:51:30 +0100

    I added Services.vue component in pages directory

---
+++ resources/assets/js/pages/Services.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Header class="header" :app="app" @onClickLogin="updateLoginPath($event)" />
+        <ServicesContent :app="app" />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import ServicesContent from "../components/ServicesContent.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+    name: "Services",
+    props: ['app'],
+    components: {
+        Header,
+        ServicesContent,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+   .header {
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 0;
+        z-index: 11;
+    }
+</style>
