623e0b538ee96cd628a24584948e5a3f72030c03
Attilius <leo20@freemail.hu>
2022-02-10 11:48:42 +0100

    I added id for select input, and I have set a click method in to confirm-btn. Next I added props array and watch hook, after that I added some method, finally I modified css

--- resources/assets/js/components/UpdateForm.vue
+++ resources/assets/js/components/UpdateForm.vue
@@ -2,11 +2,12 @@
     <div id="update-form">
         <h5>Update number of piece</h5>
         <b-form-select
+            id="select"
             class="select"
             v-model="selected"
             :options="options"
         ></b-form-select>
-        <button id="confirm-btn" class="confirm-btn">Confirm</button>
+        <b-button id="confirm-btn" @click="confirmPiece">Confirm</b-button>
         <button class="cancel" @click="cancelUpdateForm">X</button>
     </div>
 </template>
@@ -14,6 +15,30 @@
 <script>
 export default {
     name: "UpdateForm",
+    props: ["form", "updateId"],
+    watch: {
+        updateId: function(newValue) {
+            const input = document.getElementById("select");
+            const confirmBtn = document.getElementById("confirm-btn");
+
+            if (newValue && !input.value) {
+                confirmBtn.classList.add("confirm-btn-disabled");
+                confirmBtn.disabled = true;
+            }
+
+            input.addEventListener("change", e => {
+                if (newValue && e.target.value) {
+                    confirmBtn.classList.remove("confirm-btn-disabled");
+                    confirmBtn.disabled = false;
+                }
+
+                if (newValue && !e.target.value) {
+                    confirmBtn.classList.add("confirm-btn-disabled");
+                    confirmBtn.disabled = true;
+                }
+            });
+        }
+    },
 
     data() {
         return {
@@ -36,8 +61,30 @@ export default {
 
     methods: {
         cancelUpdateForm() {
-            document.getElementById("confirm-btn").style.visibility = "hidden";
-            document.getElementById("update-form").style.visibility = "hidden";
+            document.getElementById("update-form").style.display = "none";
+        },
+
+        confirmPiece() {
+            const newPiece = document.getElementById("select").value;
+            this.updatePiece(this.updateId, newPiece);
+            this.cancelUpdateForm();
+        },
+
+        updatePiece(param, newPiece) {
+            this.form.forEach((item, index) => {
+                if (param === item.state) {
+                    this.onChangePieceInForm(index, newPiece);
+                    this.onChangeUpdateState();
+                }
+            });
+        },
+
+        onChangePieceInForm(index, newPiece) {
+            this.$emit("onChangePieceInForm", [index, newPiece]);
+        },
+
+        onChangeUpdateState() {
+            this.$emit("onChangeUpdateState", false)
         }
     }
 };
@@ -49,7 +96,7 @@ export default {
     height: 200px;
     background: rgb(51, 100, 179);
     color: whitesmoke;
-    display: flex;
+    display: none;
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -58,7 +105,7 @@ export default {
     position: absolute;
     top: 900px;
     left: 400px;
-    visibility: hidden;
+    z-index: 11;
 }
 
 .select {
@@ -66,19 +113,59 @@ export default {
     margin: 10px;
 }
 
-.confirm-btn {
+#confirm-btn {
+    background: RebeccaPurple;
     width: 90px;
     height: 30px;
-    background: rebeccapurple;
-    color: whitesmoke;
-    border-radius: 0.25rem;
-    margin: 10px;
+    box-shadow: 2px 2px 5px black;
     border: none;
+    margin-left: 3px;
+    color: whitesmoke;
+    margin: 30px;
+    font-size: 1rem;
+    transition: all 0.3s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
-.confirm-btn:active {
+#confirm-btn:active {
+    box-shadow: none !important;
+    background-color: rebeccapurple !important;
+    color: whitesmoke !important;
+}
+
+#confirm-btn:hover {
     color: rebeccapurple;
     background: whitesmoke;
+    border: 1px solid rebeccapurple;
+    box-shadow: none;
+    transition: 0.4s ease-in-out 0s;
+}
+
+.confirm-btn-enabled:hover {
+    color: rebeccapurple;
+    background: whitesmoke !important;
+    border: 1px solid rebeccapurple;
+    box-shadow: none !important;
+    transition: 0.4s ease-in-out 0s;
+}
+
+.confirm-btn-enabled:active {
+    color: rebeccapurple;
+    background: whitesmoke;
+}
+
+.confirm-btn-disabled,
+.confirm-btn-disabled:hover {
+    background: rebeccapurple !important;
+    color: whitesmoke !important;
+    box-shadow: 2px 2px 5px black !important;
+    border: 1px solid rebeccapurple !important;
+    border-radius: 0.25rem;
+    margin: 10px;
+    width: 90px;
+    height: 30px;
 }
 
 .cancel {
@@ -105,4 +192,5 @@ export default {
 .confirm-btn:focus {
     box-shadow: none !important;
     border: none !important;
-}</style>
+}
+</style>
