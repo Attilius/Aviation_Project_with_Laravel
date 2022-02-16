426d629f7de5f0ba09ff51c7982ace202b8bac1f
Attilius <leo20@freemail.hu>
2022-02-16 18:26:36 +0100

    I modified api.php

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-$cities = ["/", "/london", "/new-york", "/paris"];
+$cities = ["/", "/london", "/montreal", "/new-york", "/paris"];
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
