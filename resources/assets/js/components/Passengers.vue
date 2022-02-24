5829abe800804c67ad0e2447fe5b284812cce887
Attilius <leo20@freemail.hu>
2022-02-24 22:36:16 +0100

    I modified Passengers.vue component

--- resources/assets/js/components/Passengers.vue
+++ resources/assets/js/components/Passengers.vue
@@ -1,15 +1,100 @@
 <template>
-    <div>
-
+    <div id="pasengers-box">
+        <header>
+            <h5>Passengers (1)</h5>
+        </header>
+        <div class="select-form-content">
+            <div class="passenger">
+                <div class="input-field">
+                    <i class="material-icons prefix"
+                        >account_circle</i
+                    >
+                    <input
+                        id="input-select"
+                        type="text"
+                        value="Adult"
+                        @click="onSelect"
+                    />
+                    <ul class="select-list">
+                        <li>Baby</li>
+                        <li>Child</li>
+                        <li>Adult</li>
+                        <li>Senior</li>
+                    </ul>
+                    <label for="icon_prefix" class="active">Passenger 1</label>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <b-button class="btn">Continue</b-button>
+        </footer>
     </div>
 </template>
 
 <script>
 export default {
     name: "Passengers"
-}
+};
 </script>
 
 <style scoped>
+#pasengers-box {
+    height: 40vh;
+    width: 30vw;
+    background: whitesmoke;
+    box-shadow: 2px 2px 5px #c9c9c9;
+    border-radius: 0.25rem;
+    position: absolute;
+    top: 30%;
+    left: 35%;
+    z-index: 11;
+}
 
+header {
+    height: 5vh;
+    width: 100%;
+    position: sticky;
+}
+
+header h5 {
+    color: rgb(5, 55, 115);
+    padding-left: 25px;
+    font-weight: 600;
+    text-transform: uppercase;
+}
+
+.select-form-content {
+    height: 20vh;
+    width: 100%;
+}
+
+.passenger {
+    width: 90%;
+    height: 12vh;
+    border: 2px solid #c9c9c9;
+    margin: 10px auto;
+    padding: 10px;
+}
+
+#input-select {
+    width: 20vw !important;
+}
+
+.input-field i {
+    font-size: 2.5rem;
+    color: rgb(5, 55, 115);
+}
+
+footer {
+    height: 15vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn {
+    background: rebeccapurple;
+    color: whitesmoke;
+}
 </style>
