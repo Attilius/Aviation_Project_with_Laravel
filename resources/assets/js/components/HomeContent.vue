cb573ed17fde061992d397eda294730a92f96e1a
Attilius <leo20@freemail.hu>
2022-02-28 12:53:00 +0100

    I modified HomeContent.vue component

--- resources/assets/js/components/HomeContent.vue
+++ resources/assets/js/components/HomeContent.vue
@@ -29,7 +29,19 @@
                                 <input
                                     id="departure_place"
                                     type="text"
+                                    @click="test"
                                 />
+                                <ul class="airports">
+                                    <li v-for="airport in airports" :key="airport.id">
+                                        <div class="airport">
+                                            <i id="rotate-90deg" class="material-icons">flight</i>
+                                            <div class="data_">
+                                                <span id="city_">{{airport.city}}, {{airport.airport}}</span>
+                                                <span id="country_">{{airport.IATA_code}}-{{airport.country}}</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
                                 <label id="label-departure" for="from"
                                     >Departure from*</label
                                 >
@@ -194,7 +206,8 @@
 
 <script>
 import Chat from "./Chat.vue";
-import Passengers from "./Passengers.vue"
+import Passengers from "./Passengers.vue";
+import AirportsDatas from "../../json/airports.json";
 export default {
     name: "HomeContent",
     props: ["app"],
@@ -205,6 +218,7 @@ export default {
 
     data() {
         return {
+            airports: AirportsDatas.airports,
             selected_radio: "",
             passengers_value: "1 Adult",
             cityUri: ""
@@ -222,6 +236,9 @@ export default {
     },
 
     methods: {
+        test() {
+            document.getElementsByClassName("airports")[0].style.display = "block";
+        },
         updatePassengersValue(updatePassengersValue) {
             this.passengers_value = updatePassengersValue;
         },
@@ -610,6 +627,55 @@ input:focus {
 .passengers {
     display: none;
 }
+
+.airports {
+    width: 450px;
+    height: 200px;
+    border: 1px solid grey;
+    overflow-y: scroll;
+    display: none;
+}
+
+.airports li {
+    height: 50px;
+    width: 100%;
+    border-top: 1px solid #ced4da;
+}
+
+.airport {
+    height: 50px;
+    width: 100%;
+    display: flex;
+}
+
+.airport i {
+    width: 50px;
+    height: 50px;
+    padding: 5px;
+}
+
+#rotate-90deg {
+    transform: rotate(90deg);
+    color: rgb(5, 55, 115);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.data_ {
+    width: 80%;
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: 12px;
+};
+
+#city_, #country_ {
+    margin-bottom: 0 !important;
+}
+
+
 
 /*-------- Main content settings ------*/
 
