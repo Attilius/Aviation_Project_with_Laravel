aec6c2ee6ca3420fe78de42b0ddf8289d9391f98
Attilius <leo20@freemail.hu>
2022-03-06 18:23:28 +0100

    I modified PageNotFound.vue component

--- resources/assets/js/pages/PageNotFound.vue
+++ resources/assets/js/pages/PageNotFound.vue
@@ -2,16 +2,18 @@
     <div>
         <Header class="header" :app="app" @onClickLogin="updateLoginPath($event)" />
         <div class="content">
-            <h1>Page is Not found :(</h1>
+            <div class="shadow">
+                <h1>Sorry! This page is not found...</h1>
+                <router-link class="btn" to="/">Back to Homepage</router-link>
+            </div>
         </div>
-        <Footer />
+        <Footer :app="app" />
     </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
-
 export default {
     name: "PageNotFound",
     props: ['app'],
@@ -34,5 +36,35 @@ export default {
 .content {
     height: 100vh;
     width: 100%;
+    background-image: url("/images/page-not-found_bg.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
 }
+
+.shadow {
+    height: 100vh;
+    width: 100%;
+    background: rgba(1, 1, 1, .4);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.shadow h1 {
+    color: whitesmoke;
+}
+
+.shadow .btn {
+    background: rebeccapurple;
+    transition: all .5s ease 0s;
+}
+
+.shadow .btn:hover {
+    background: whitesmoke;
+    color: rebeccapurple;
+    transition: all .5s ease 0s;
+}
+
 </style>
