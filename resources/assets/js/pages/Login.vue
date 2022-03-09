fc9f823b354e80412b0ebbfce210c287b79b4ecc
Attilius <leo20@freemail.hu>
2022-03-09 22:15:21 +0100

    I modified Login.vue component

--- resources/assets/js/pages/Login.vue
+++ resources/assets/js/pages/Login.vue
@@ -1,33 +1,56 @@
 <template>
-    <div class="container">
-        <div class="alert alert-danger" v-if="errors.length">
-            <ul>
-                <li v-for="(error, index) in errors" :key="index">
-                    {{error}}
-                </li>
-            </ul>
+    <div class="login-container">
+        <div class="skin">
+            <div class="alert alert-danger" v-if="errors.length">
+                <ul>
+                    <li v-for="(error, index) in errors" :key="index">
+                        {{ error }}
+                    </li>
+                </ul>
+            </div>
+            <div class="login-form">
+                <b-img
+                    class="logo"
+                    :src="'../images/logo.png'"
+                    alt="logo"
+                ></b-img>
+                <b-form id="form" @submit.prevent="onSubmit">
+                    <div class="input-field">
+                        <input id="email" type="email" class="validate" />
+                        <label for="email">Email*</label>
+                        <span
+                            class="helper-text"
+                            data-error="wrong"
+                            ></span
+                        >
+                    </div>
+                    <div class="input-field">
+                        <input id="password" type="password" class="validate" />
+                        <label for="password">Password*</label>
+                        <span
+                            class="helper-text"
+                            data-error="wrong"
+                            ></span
+                        >
+                    </div>
+                    <input type="submit" value="Login" />
+                </b-form>
+            </div>
         </div>
-        <b-form @submit.prevent="onSubmit">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" v-model="email">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" v-model="password">
-            <input type="submit" value="Login">
-        </b-form>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Login',
-    props: ['app'],
+    name: "Login",
+    props: ["app"],
 
     data() {
         return {
             email: "",
             password: "",
             errors: []
-        }
+        };
     },
 
     methods: {
@@ -46,25 +69,60 @@ export default {
                 const data = {
                     email: this.email,
                     password: this.password
-                }
+                };
 
-                this.app.req.post('auth/login', data).then(response => {
-                    this.app.user = response.data;
-                    this.$router.push(loginPath);
-                }).catch(error => {
-                    this.errors.push(error.response.data.error);
-                });
+                this.app.req
+                    .post("auth/login", data)
+                    .then(response => {
+                        this.app.user = response.data;
+                        this.$router.push(loginPath);
+                    })
+                    .catch(error => {
+                        this.errors.push(error.response.data.error);
+                    });
             }
         }
-    } 
-}
+    }
+};
 </script>
 
 <style scoped>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    };
+.login-container {
+    background-image: url("../../img/login-register_bg.jpg");
+    background-position: center;
+    background-size: cover;
+    height: 100vh;
+    width: 100%;
+}
+
+.skin {
+    height: 100vh;
+    width: 100%;
+    background: rgba(1, 1, 1, 0.6);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.login-form {
+    height: 60%;
+    width: 30%;
+    border: 1px solid whitesmoke;
+    border-radius: 0.25rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: rgba(5, 55, 115, 0.7);
+}
+
+.logo {
+    height: 150px;
+    width: 150px;
+}
+
+#form .input-field {
+    width: 100%;
+}
 </style>
