5638eea22b41837a35c684dbfc15fcfcce22462f
Attilius <leo20@freemail.hu>
2022-03-23 21:18:47 +0100

    I modified UserProfile.vue component

--- resources/assets/js/pages/UserProfile.vue
+++ resources/assets/js/pages/UserProfile.vue
@@ -3,32 +3,51 @@
         <header>
             <h4>Edit your profile</h4>
         </header>
+        <section>
+            <div class="menu"></div>
+            <div class="content"></div>
+        </section>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'UserProfile'
-}
+    name: "UserProfile"
+};
 </script>
 
 <style scoped>
-    .wrapper {
-        height: 100vh;
-        width: 100%;
-        background: rgb(5,55,115);
-    }
+.wrapper {
+    height: 100vh;
+    width: 100%;
+    background: rgb(5, 55, 115);
+}
 
-    header {
-        width: 70%;
-        height: 80px;
-        border-bottom: 1px solid whitesmoke;
-        padding-top: 10px;
-        margin: 0 auto;
-    }
+header {
+    width: 70%;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+    padding-top: 10px;
+    margin: 0 auto;
+}
 
-    header h4 {
-        color: whitesmoke;
-        margin: 0;
-    }
+header h4 {
+    color: whitesmoke;
+    margin: 0;
+}
+
+section {
+    display: flex;
+}
+
+.menu {
+    height: 100%;
+    width: 20%;
+    border-right: 1px solid whitesmoke;
+}
+
+.content {
+    height: 100%;
+    width: 80%;
+}
 </style>
