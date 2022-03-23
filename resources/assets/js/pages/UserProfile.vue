f55a89c0271c15f92a3a73b97182351451324f59
Attilius <leo20@freemail.hu>
2022-03-23 20:53:35 +0100

    I modified UserProfile.vue component

--- resources/assets/js/pages/UserProfile.vue
+++ resources/assets/js/pages/UserProfile.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="wrapper">
-        
+        <header>
+            <h4>Edit your profile</h4>
+        </header>
     </div>
 </template>
 
@@ -15,5 +17,18 @@ export default {
         height: 100vh;
         width: 100%;
         background: rgb(5,55,115);
+    }
+
+    header {
+        width: 70%;
+        height: 80px;
+        border-bottom: 1px solid whitesmoke;
+        padding-top: 10px;
+        margin: 0 auto;
+    }
+
+    header h4 {
+        color: whitesmoke;
+        margin: 0;
     }
 </style>
