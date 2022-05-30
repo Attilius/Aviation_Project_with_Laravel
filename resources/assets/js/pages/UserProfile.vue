440178feec86214823ff87f350e251df26575d10
Attilius <leo20@freemail.hu>
2022-05-30 21:05:11 +0200

    I modified UserProfile.vue

--- resources/assets/js/pages/UserProfile.vue
+++ resources/assets/js/pages/UserProfile.vue
@@ -8,6 +8,7 @@
                 <ul>
                     <li> <a href="/">Menu-1</a></li>
                     <li> <a href="/">Menu-2</a></li>
+                    <li> <a href="/">Menu-3</a></li>
                     <li> <a href="/">Delete account</a></li>
                 </ul>
             </div>
