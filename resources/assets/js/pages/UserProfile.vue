6a7968f8b3d1eacec609e8b2795feee7176dd59e
Attilius <leo20@freemail.hu>
2022-05-31 21:39:58 +0200

    I modified UserProfile.vue

--- resources/assets/js/pages/UserProfile.vue
+++ resources/assets/js/pages/UserProfile.vue
@@ -8,7 +8,7 @@
                 <ul>
                     <li> <a href="/">Menu-1-</a></li>
                     <li> <a href="/">Menu-2-</a></li>
-                    <li> <a href="/">Menu-3</a></li>
+                    <li> <a href="/">Menu-3-</a></li>
                     <li> <a href="/">Menu-4</a></li>
                     <li> <a href="/">Menu-5</a></li>
                     <li> <a href="/">Menu-6</a></li>
