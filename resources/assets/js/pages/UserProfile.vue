c64399331fc6bd6ab2855acd1cd3bbe8e8f6c7bf
Attilius <leo20@freemail.hu>
2022-06-01 20:51:54 +0200

    I modified UserProfile.vue

--- resources/assets/js/pages/UserProfile.vue
+++ resources/assets/js/pages/UserProfile.vue
@@ -6,7 +6,7 @@
         <section>
             <div class="menu">
                 <ul>
-                    <li> <a href="/">Menu-1-</a></li>
+                    <li> <a href="/">Favorit places</a></li>
                     <li> <a href="/">Menu-2-</a></li>
                     <li> <a href="/">Menu-3-</a></li>
                     <li> <a href="/">Menu-4-</a></li>
